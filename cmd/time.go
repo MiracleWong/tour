564a5906da16b42c1c050502a7a104e3d937453e
MiracleWong <cfwr1991@126.com>
2020-10-26 19:00:57 +0800

    mofify the time.Time.Format

--- cmd/time.go
+++ cmd/time.go
@@ -1,13 +1,19 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+    "github.com/MiracleWong/tour/internal/timer"
+    "github.com/spf13/cobra"
+    "log"
+)
 
 var timeCmd = &cobra.Command{
 	Use:   "time",
 	Short: "时间格式处理",
 	Long:  "时间格式处理",
 	Run: func(cmd *cobra.Command, args []string) {
-		println("Hello World")
+        nowTime := timer.GetNowTime()
+        log.Printf("输出结果：%s, %d", nowTime.Format("2006-01-02 15:04:06"), nowTime.Unix())
+        //log.Printf("输出结果：%s, %d", nowTime, nowTime.Unix())
 	},
 }
 
