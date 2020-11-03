eacd4b27d09f4cb59e6a17fcad2d58bbc6cf067b
MiracleWong <cfwr1991@126.com>
2020-11-03 09:58:12 +0800

    表字段的类型映射 &  针对columns 表进行查询和数据组装

--- internal/sql2struct/mysql.go
+++ internal/sql2struct/mysql.go
@@ -2,6 +2,7 @@ package sql2struct
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	//导入驱动
 	_ "github.com/go-sql-driver/mysql"
@@ -32,6 +33,35 @@ type TableColumn struct {
 	ColumnComment string
 }
 
+// 表字段的类型映射
+var DBTypeToStructType = map[string]string{
+	"int":        "int32",
+	"tinyint":    "int8",
+	"smallint":   "int",
+	"mediumint":  "int64",
+	"bigint":     "int64",
+	"bit":        "int",
+	"bool":       "bool",
+	"enum":       "string",
+	"set":        "string",
+	"varchar":    "string",
+	"char":       "string",
+	"tinytext":   "string",
+	"mediumtext": "string",
+	"text":       "string",
+	"longtext":   "string",
+	"blob":       "string",
+	"tinyblob":   "string",
+	"mediumblob": "string",
+	"longblob":   "string",
+	"date":       "time.Time",
+	"datetime":   "time.Time",
+	"timestamp":  "time.Time",
+	"time":       "time.Time",
+	"float":      "float64",
+	"double":     "float64",
+}
+
 // Go 中，连接数据库的方法
 func (m *DBModel) Connect() error {
 	var err error
@@ -50,4 +80,31 @@ func (m *DBModel) Connect() error {
 		return err
 	}
 	return nil
+}
+
+// 针对columns 表进行查询和数据组装
+func (m *DBModel) GetColumns(dbName, tableName string) ([]*TableColumn, error) {
+	query := "SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY, " +
+		"IS_NULLABLE, COLUMN_TYPE, COLUMN_COMMENT " +
+		"FROM COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? "
+	rows, err := m.DBEngine.Query(query, dbName, tableName)
+	if err != nil {
+		return nil, err
+	}
+	if rows == nil {
+		return nil, errors.New("没有数据")
+	}
+	defer rows.Close()
+	var columns []*TableColumn
+
+	for rows.Next() {
+		var column TableColumn
+		err := rows.Scan(&column.ColumnName, &column.DataType, &column.ColumnKey,
+			&column.IsNullable, &column.ColumnType, &column.ColumnComment)
+		if err != nil {
+			return nil, err
+		}
+		columns = append(columns, &column)
+	}
+	return columns, nil
 }
