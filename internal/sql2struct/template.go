ce8fd5e40857df6547f6da22dd7def514db4360f
MiracleWong <cfwr1991@126.com>
2020-11-05 14:55:39 +0800

    加入解析的模板structTpl，3 个结构体：StructTemplateDB：用于渲染的模板对象信息、StructColumn：存储转换后的 Go 结构体的所有字段信息、StructTemplate

--- internal/sql2struct/template.go
+++ internal/sql2struct/template.go
@@ -1,12 +1,19 @@
 package sql2struct
 
+//const structTpl = `type {{.TableName | ToCamelCase}} struct {
+//{{range .Columns}}	{{ $length := len .Comment}} {{ if gt $length 0 }}// {{.Comment}} {{else}}// {{.Name}} {{ end }}
+//	{{ $typeLen := len .Type }} {{ if gt $typeLen 0 }}{{.Name | ToCamelCase}}	{{.Type}}	{{.Tag}}{{ else }}{{.Name}}{{ end }}
+//{{end}}}
+//func (model {{.TableName | ToCamelCase}}) TableName() string {
+//	return "{{.TableName}}"
+//}`
 const structTpl = `type {{.TableName | ToCamelCase }} struct {
 {{range .Columns}}
 	{{ $length := len .Comment}}
 	{{ if gt $length 0}}
-    // {{.Comment}}
+ // {{.Comment}}
 	{{ else }}
-	// {{.Name}} 	
+	// {{.Name}}
 	{{ end }}
 	{{ $typeLen := len .Type }}
 	{{ if gt $typeLen 0 }}
@@ -21,3 +28,21 @@ func (model {{.TableName | ToCamelCase }}) TableName()  string {
 	return "{{.TableName}}"
 }
 `
+
+type StructTemplate struct {
+	structTpl string
+}
+
+// 用于存储转换后的 Go 结构体的所有字段信息
+type StructColumn struct {
+	Name    string
+	Type    string
+	Tag     string
+	Comment string
+}
+
+// 存储最终用于渲染的模板对象信息
+type StructTemplateDB struct {
+	TableName string
+	Columns   []*StructColumn
+}
