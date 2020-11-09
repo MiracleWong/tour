28de02362706193ce5bd7fdf6d7ade0deae9b675
MiracleWong <cfwr1991@126.com>
2020-11-09 11:03:58 +0800

    声明 cmd 变量，测试 sql 和 struct 命令

--- cmd/sql.go
+++ cmd/sql.go
@@ -1,17 +1,35 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
-	"log"
 )
+
+// 声明7各 cmd 全局变量。用于接收外部的命令行参数
+var username string
+var password string
+var host string
+var charset string
+var dbType string
+var dbName string
+var tableName string
 
 var sqlCmd = &cobra.Command{
 	Use:   "sql",
 	Short: "sql 转换和处理",
 	Long:  "sql 转换和处理",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+var sql2structCmd = &cobra.Command{
+	Use:   "struct",
+	Short: "sql 转换",
+	Long:  "sql 转换",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("Hello World")
+		fmt.Println("struct ")
 	},
 }
 
-func init() {}
+func init() {
+	sqlCmd.AddCommand(sql2structCmd)
+}
