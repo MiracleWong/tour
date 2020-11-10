ac4a9da1430c6dc7676393d6c4a674a1b637db26
MiracleWong <cfwr1991@126.com>
2020-11-10 19:52:53 +0800

    分解和转换：AssemblyColumns，对模板的渲染和处理：Generate

--- internal/sql2struct/template.go
+++ internal/sql2struct/template.go
@@ -1,5 +1,12 @@
 package sql2struct
 
+import (
+	"fmt"
+	"github.com/MiracleWong/tour/internal/word"
+	"os"
+	"text/template"
+)
+
 //const structTpl = `type {{.TableName | ToCamelCase}} struct {
 //{{range .Columns}}	{{ $length := len .Comment}} {{ if gt $length 0 }}// {{.Comment}} {{else}}// {{.Name}} {{ end }}
 //	{{ $typeLen := len .Type }} {{ if gt $typeLen 0 }}{{.Name | ToCamelCase}}	{{.Type}}	{{.Tag}}{{ else }}{{.Name}}{{ end }}
@@ -7,11 +14,12 @@ package sql2struct
 //func (model {{.TableName | ToCamelCase}}) TableName() string {
 //	return "{{.TableName}}"
 //}`
+//模板对象声明
 const structTpl = `type {{.TableName | ToCamelCase }} struct {
 {{range .Columns}}
 	{{ $length := len .Comment}}
 	{{ if gt $length 0}}
- // {{.Comment}}
+// {{.Comment}}
 	{{ else }}
 	// {{.Name}}
 	{{ end }}
@@ -45,4 +53,40 @@ type StructColumn struct {
 type StructTemplateDB struct {
 	TableName string
 	Columns   []*StructColumn
+}
+
+func NewStructTemplate() *StructTemplate {
+	return &StructTemplate{structTpl: structTpl}
+}
+
+// 通过对查询 COLUMNS 表所组装得到的tbColumns，进行进一步的分解和转换
+func (t *StructTemplate) AssemblyColumns(tbColumns []*TableColumn) []*StructColumn {
+	tplColumns := make([]*StructColumn, 0, len(tbColumns))
+	for _, column := range tbColumns {
+		tag := fmt.Sprintf("`"+"json:"+"\"%s\""+"`", column.ColumnName)
+		tplColumns = append(tplColumns, &StructColumn{
+			Name:    column.ColumnName,
+			Type:    DBTypeToStructType[column.DataType],
+			Tag:     tag,
+			Comment: column.ColumnComment,
+		})
+	}
+	return tplColumns
+}
+
+// 对模块渲染的自定义函数和模板对象处理
+func (t *StructTemplate) Generate(tableName string, tplColumns []*StructColumn) error {
+	tpl := template.Must(template.New("sql2struct").Funcs(template.FuncMap{
+		"ToCamelCase": word.UnderscoreToUpperCamelCase,
+	}).Parse(t.structTpl))
+
+	tplDB := StructTemplateDB{
+		TableName: tableName,
+		Columns:   tplColumns,
+	}
+	err := tpl.Execute(os.Stdout, tplDB)
+	if err != nil {
+		return err
+	}
+	return nil
 }
