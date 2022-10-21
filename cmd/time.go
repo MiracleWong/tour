21a1f33a3b531d80df4d614d48d9dd8c120e3d30
MiracleWong <cfwr1991@126.com>
2022-10-21 17:11:02 +0800

    [功能]：cobra 子命令：time calc，计算时间

--- cmd/time.go
+++ cmd/time.go
@@ -4,8 +4,13 @@ import (
 	"github.com/MiracleWong/tour/internal/timer"
 	"github.com/spf13/cobra"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 )
+
+var calculateTime string
+var duration string
 
 var timeCmd = &cobra.Command{
 	Use:   "time",
@@ -27,6 +32,39 @@ var nowTimeCmd = &cobra.Command{
 	},
 }
 
+var calcTimeCmd = &cobra.Command{
+	Use:   "calc",
+	Short: "计算所需时间",
+	Long:  "计算所需时间",
+	Run: func(cmd *cobra.Command, args []string) {
+		var currentTimer time.Time
+		var layout = "2006-01-02 15:04:05"
+		if calculateTime == "" {
+			currentTimer = timer.GetNowTime()
+		} else {
+			var err error
+			if !strings.Contains(calculateTime, " ") {
+				layout = "2006-01-02"
+			}
+			currentTimer, err = time.Parse(layout, calculateTime)
+			if err != nil {
+				t, _ := strconv.Atoi(calculateTime)
+				currentTimer = time.Unix(int64(t), 0)
+			}
+		}
+
+		calculateTime, err := timer.GetCalculateTime(currentTimer, duration)
+		if err != nil {
+			log.Printf("CalculateTime error : %v", err)
+		}
+		log.Printf("输出结果：%s, %d", calculateTime.Format(layout), calculateTime.Unix())
+	},
+}
+
 func init() {
 	timeCmd.AddCommand(nowTimeCmd)
+	timeCmd.AddCommand(calcTimeCmd)
+
+	calcTimeCmd.Flags().StringVarP(&calculateTime, "calculateTime", "c", "", "需要计算的时间")
+	calcTimeCmd.Flags().StringVarP(&duration, "duration", "d", "", "持续时间")
 }
--- internal/timer/time.go
+++ internal/timer/time.go
@@ -7,3 +7,13 @@ import (
 func GetNowTime() time.Time {
 	return time.Now()
 }
+
+// 计算时间
+
+func GetCalculateTime(currentTimer time.Time, d string) (time.Time, error) {
+	duration, err := time.ParseDuration(d)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return currentTimer.Add(duration), nil
+}
